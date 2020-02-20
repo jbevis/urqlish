78ccc50d28536b99c8ac970500c6727397c3e253
Jack Bevis <jack.bevis@gmail.com>
2020-02-20 16:15:10 -0700

    sic parvis magna

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+const cache = cacheExchange();
+const client = new Client({
+  url: 'http://localhost:4000',
+  exhcanges: [dedupExchange, cache, fetchExchange]
+});
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
