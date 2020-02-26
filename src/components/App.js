4a04c0d50dd75861cc330d76ad880e15dc75b5e0
Jack Bevis <jack.bevis@gmail.com>
2020-02-25 23:55:07 -0700

    working on authentication

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import Header from './Header';
+import Login from './Login';
 
 const App = () => (
   <div>
     <Header />
     <div className='ph3 pv1 background-gray'>
       <Switch>
-        <Route exact path="/" component={LinkList} />
-        <Route exact paht="/create" component={CreateLink} />
+        <Route exact path='/' component={LinkList} />
+        <Route exact path='/create' component={CreateLink} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </div>
   </div>
